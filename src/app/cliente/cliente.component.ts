47176a3e502a4a7ef0eb4b3e074ef6417359addb
orsolon <56239735+orsolon@users.noreply.github.com>
2021-07-16 23:03:41 -0300

    listar no

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Usuario } from './usuario.model';
+import { User } from './usuario.model';
 
 @Component({
   selector: 'app-cliente',
@@ -8,32 +8,49 @@ import { Usuario } from './usuario.model';
 })
 export class ClienteComponent implements OnInit {
 
-  usuarios: Usuario[] = [{
-    id: 1,
-    name: "Debora Orsolon",
-    cpf: "333.333.111-30",
-    email: "debora@gmail.com",
-    password: '123'
-  },
-  {
-    id: 2,
-    name: "Marco Antonio",
-    cpf: "111.333.111-30",
-    email: "marco@gmail.com",
-    password: '300400'
-  },
-  {
-    id: 3,
-    name: "Rodrigo Vargas",
-    cpf: "612.321.111-30",
-    email: "rodvargas@gmail.com",
-    password: '9878'
-  },
-]
+  users: User[] = [];
+
+//   users: User[] = [{
+//     id: 1,
+//     name: "Debora Orsolon",
+//     cpf: "333.333.111-30",
+//     email: "debora@gmail.com",
+//     password: '123'
+//   },
+//   {
+//     id: 2,
+//     name: "Marco Antonio",
+//     cpf: "111.333.111-30",
+//     email: "marco@gmail.com",
+//     password: '300400'
+//   },
+//   {
+//     id: 3,
+//     name: "Rodrigo Vargas",
+//     cpf: "612.321.111-30",
+//     email: "rodvargas@gmail.com",
+//     password: '9878'
+//   },
+//   {
+//     id: 4,
+//     name: "Maria da Dores ",
+//     cpf: "123.321.555-30",
+//     email: "maria@gmail.com",
+//     password: '9876783'
+//   },
+// ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onUserCreated(event: any) {
+    this.users.push(event);
+    this.users.forEach((user, i) => {
+      user.id = i + 1;
+    })
+    console.log(this.users);
   }
 
 }
--- src/app/cliente/formulario-cliente/formulario-cliente.component.ts
+++ src/app/cliente/formulario-cliente/formulario-cliente.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Usuario } from '../usuario.model';
+import { User } from '../usuario.model';
 
 @Component({
   selector: 'app-formulario-cliente',
@@ -9,14 +9,13 @@ import { Usuario } from '../usuario.model';
 })
 export class FormularioClienteComponent implements OnInit {
   myForm!: FormGroup;
-  usuarios: Usuario[] = [];
+  users: User[] = [];
 
-  @Output() createUser = new EventEmitter<any>();
+   @Output() icreateUser = new EventEmitter<any>();
 
   constructor(private formBuilder: FormBuilder) { }
 
     ngOnInit(): void {
-      console.log("teste")
       this.myForm = new FormGroup({
         name: new FormControl("", Validators.required),
         cpf: new FormControl("", Validators.required),
@@ -29,11 +28,19 @@ export class FormularioClienteComponent implements OnInit {
     });
   }
 
-  onSubmit() {
+  createUser() {
       console.log("Form Submitted!", this.myForm.value);
-      this.usuarios.push(this.myForm.value);
-      this.createUser.emit(this.myForm.value);
+      this.users.push(this.myForm.value);
+      this.icreateUser.emit(this.myForm.value);
       this.myForm.reset();
+  }
+  
+  onUserCreated(event: any) {
+    console.log(event);
+    this.users.push(event);
+    this.users.forEach((user, i) => {
+      user.id = i + 1;
+    })
   }
   
 
