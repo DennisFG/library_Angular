581c2888f067830953ce30ae1e1c161ecd40f6d8
DennisFG <84360092+DennisFG@users.noreply.github.com>
2021-07-20 22:17:41 -0300

    adicionado routerLink para os botões da navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AluguelComponent } from './books/aluguel/aluguel.component';
 import { BooksComponent } from './books/books.component';
+import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { UserComponent } from './user/user.component';
@@ -10,10 +11,11 @@ const routes: Routes = [
   { path: '', component: HomePageComponent },
   {
     path: 'books', component: BooksComponent, children: [
-      { path: 'aluguel', component: AluguelComponent },
+      // { path: 'rent', component: AluguelComponent },
     ]
   },
   { path: 'user', component: UserComponent },
+  { path: 'login', component: LoginComponent },
   // abaixo, inserir página com validação
   // {
   //   path: 'usuarios', canActivate: [AuthGuard], component: UsersComponent, children: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserComponent } from './user/user.component';
 import { FormUserComponent } from './user/form-user/form-user.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     BtnCreateComponent,
     UserComponent,
     FormUserComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LoginComponent
 
   ],
   imports: [
