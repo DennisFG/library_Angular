b8d3e4487923b3762cb7801465e059078d47b219
Débora Mello <debora.mello@upflow.me>
2021-07-23 21:21:31 -0300

    Consumindo API de livros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { LoginComponent } from './components/login/login.component';
 import { RentComponent } from './components/books/rent/rent.component';
 import { BtnCreateComponent } from './components/books/rent/btn-create/btn-create.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -36,7 +38,8 @@ import { BtnCreateComponent } from './components/books/rent/btn-create/btn-creat
     ModalModule.forRoot(),
     BsDatepickerModule.forRoot(),
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
